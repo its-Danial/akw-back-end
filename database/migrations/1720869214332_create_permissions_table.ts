f36fc73d6b15ce50bceab7a984385d963ab1910a
its-Danial <itsmalikdanial@gmail.com>
2024-07-14 11:36:39 +0500

    added can_view permission

--- database/migrations/1720869214332_create_permissions_table.ts
+++ database/migrations/1720869214332_create_permissions_table.ts
@@ -7,6 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table.boolean('can_view').defaultTo(true)
       table.boolean('can_edit').defaultTo(true)
       table.boolean('can_delete').defaultTo(true)
       table.boolean('can_view_all').defaultTo(false)
